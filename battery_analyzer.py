09e732fe1853919dbfeebf2b53ab59f4adc2cc42
Kirill Voronin <yoursdearboy@gmail.com>
2019-09-23 14:16:58 +0300

    Added python script to analyze battery usage

---
+++ battery_analyzer.py
@@ -0,0 +1,75 @@
+#!/usr/bin/env python
+# Script to analyze battery
+# requires pandas and bokeh
+#   pip install pandas bokeh
+
+import os
+import argparse
+import datetime
+import pandas as pd
+from bokeh.models import ColumnDataSource
+from bokeh.plotting import figure, show
+
+
+DEFAULT_LOG_FILE_PATH = os.path.join(os.environ.get('HOME', ''), '.battery.log')
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument('logfile', nargs='?', type=argparse.FileType('r'), default=DEFAULT_LOG_FILE_PATH)
+parser.add_argument('param', type=str)
+args = parser.parse_args()
+
+
+def read_log_chunk(f):
+    acc = []
+    empty = False
+    for line in f:
+        if line.strip():
+            acc.append(line.strip())
+        else:
+            if empty:
+                yield acc
+                acc = []
+                empty = False
+            else:
+                empty = True
+
+
+def extract_param(data, param):
+    return_value = None
+    for line in data:
+        content = line.split(' = ', 1)
+        if len(content) < 2:
+            continue
+        key, value = content[0], content[1]
+        key = key.strip('"')
+        if key == param:
+            if return_value is not None:
+                print(data)
+                raise Exception("Multiple keys matched when extracting \"%s\" param" % param)
+            return_value = value
+    return return_value
+
+
+def parse_int(value):
+    return int(value) if value is not None else None
+
+
+df = []
+for chunk in read_log_chunk(args.logfile):
+    date = datetime.datetime.strptime(chunk[0], '%Y-%m-%d %H:%M:%S')
+    data = chunk[1:]
+    df.append(dict(
+        date=date,
+        AvgTimeToEmpty=parse_int(extract_param(data, 'AvgTimeToEmpty')),
+        Temperature=parse_int(extract_param(data, 'Temperature'))
+    ))
+df = pd.DataFrame(df)
+
+args.logfile.close()
+
+source = ColumnDataSource(df)
+p = figure(x_axis_type="datetime", plot_width=800, plot_height=350)
+p.yaxis.axis_label = args.param
+p.line('date', args.param, source=source)
+show(p)
